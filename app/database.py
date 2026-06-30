ef02c426f775cf025445ec40f4172488ba73903b
Meet Sharma <meetsharma1569@gmail.com>
2026-06-30 18:13:54 +0530

    Refactor database connection to use environment variable

--- app/database.py
+++ app/database.py
@@ -1,17 +1,16 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./placement.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-DATABASE_URL,
-connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
-autocommit=False,
-autoflush=False,
-bind=engine
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
 
 Base = declarative_base()
